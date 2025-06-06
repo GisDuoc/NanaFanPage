827bbeb04f9732f23b4ade0b3c216cd22014bb9f
Gisbel <gisb.contreras@duocuc.cl>
2025-06-05 22:16:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NanaFanPage',
+  webDir: 'www'
+};
+
+export default config;
